241d889d2dd6e52a72cc0ec9744be0b0b467f80a
Nick Soan <nick.soan@parker.com>
2021-09-28 10:28:01 +0100

    Initial Commit

---
+++ EmailReader/Credentials.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailReader
+{
+    public class Credentials
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+
+
+        public Credentials()
+        {
+            UserName = "nick.soan@parker.com";
+            Password = "xxxxxxxxxxxxxxx";
+        }
+    }
+
+    
+}
---
+++ EmailReader/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.Exchange.WebServices.Data;
+
+namespace EmailReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExchangeService _service;
+            int itemsToRetrieve = 5;
+            try
+            {
+                Console.WriteLine("Registering Exchange connection");
+
+                var creds = new Credentials();
+                _service = new ExchangeService
+                {
+                    Credentials = new WebCredentials(creds.UserName, creds.Password)
+                };
+            }
+            catch
+            {
+                Console.WriteLine("Connection to Exchange failed.");
+                return;
+            }
+
+            _service.Url = new Uri("https://outlook.office365.com/EWS/Exchange.asmx");
+
+            try
+            {
+                Console.WriteLine("Reading mail");
+
+                var findResults = _service.FindItems(WellKnownFolderName.Inbox, new ItemView(itemsToRetrieve));
+                foreach (Item item in findResults.Items)
+                {
+                    EmailMessage message = EmailMessage.Bind(_service, item.Id, new PropertySet(ItemSchema.Attachments));
+
+                    message.Load();
+                    Console.WriteLine("=============================================================");
+                    Console.WriteLine($"Message from {message.From}. Attachment Count: {message.Attachments.Count}");
+                    //Console.WriteLine($"{message.Body.Text}");
+                }
+
+                Console.WriteLine("Done.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error has occured. \n:" + e.Message);
+            }
+        }
+    }
+}
